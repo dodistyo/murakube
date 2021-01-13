fd94ccb119c487a6ffc658ad773c20e1af5fccb3
Dodi <dodipras27@gmail.com>
2021-01-13 14:06:20 +0700

    handle swap back android

--- lib/src/index_screen.dart
+++ lib/src/index_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:murakube/src/models/tabIcon_data.dart';
@@ -70,23 +71,26 @@ class IndexScreenState extends State<IndexScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: MurakubeAppTheme.background,
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Stack(
-            children: <Widget>[
-              tabBody,
-              (() {
-                if (loggedin) {
-                  return bottomBar();
-                } else {
-                  return Column();
-                }
-              }()),
-            ],
-          ),
-        ));
+    return new WillPopScope(
+      onWillPop: _onWillPop,
+      child: Container(
+          color: MurakubeAppTheme.background,
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Stack(
+              children: <Widget>[
+                tabBody,
+                (() {
+                  if (loggedin) {
+                    return bottomBar();
+                  } else {
+                    return Column();
+                  }
+                }()),
+              ],
+            ),
+          )),
+    );
   }
 
   Future<bool> getData() async {
@@ -143,5 +147,33 @@ class IndexScreenState extends State<IndexScreen>
         ),
       ],
     );
+  }
+
+  // this is the future function called to show dialog for confirm exit.
+  Future<bool> _onWillPop() {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Confirm Exit?',
+                style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+            content: new Text(
+                'Are you sure you want to exit the app? Tap \'Yes\' to exit \'No\' to cancel.'),
+            actions: <Widget>[
+              new FlatButton(
+                onPressed: () {
+                  // this line exits the app.
+                  SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                },
+                child: new Text('Yes', style: new TextStyle(fontSize: 18.0)),
+              ),
+              new FlatButton(
+                onPressed: () =>
+                    Navigator.pop(context), // this line dismisses the dialog
+                child: new Text('No', style: new TextStyle(fontSize: 18.0)),
+              )
+            ],
+          ),
+        ) ??
+        false;
   }
 }
